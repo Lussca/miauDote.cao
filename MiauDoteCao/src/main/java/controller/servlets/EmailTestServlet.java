dae3dac97bc680e938b262e7a153164ef60ccec6
JoaoGabriel19 <gabrielsilva79@live.com>
2023-06-14 16:24:36 -0300

    Testes de Email

--- MiauDoteCao/src/main/java/controller/servlets/EmailTestServlet.java
+++ MiauDoteCao/src/main/java/controller/servlets/EmailTestServlet.java
@@ -1,11 +1,19 @@
 package controller.servlets;
 
 import java.io.IOException;
+
+import controller.handler.email.CreateEmail;
+import controller.handler.email.CreateMessage;
 import controller.handler.email.EmailSender;
+import controller.handler.email.SendMessage;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.api.services.gmail.model.Message;
 
 /**
  * Servlet implementation class EmailTestServlet
@@ -34,7 +42,13 @@ public class EmailTestServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email");
-		EmailSender.sendValidationEmail(email, "CODIGO DE VALIDACAO");
+		try {
+			MimeMessage mimeMessage = CreateEmail.createEmail(email, "miaudote.cao@gmail.com", "Codigo de verificacao", "Aqui esta seu codigo de verificacao: 1234");
+			Message message = CreateMessage.createMessageWithEmail(mimeMessage);
+			SendMessage.sendEmail("miauDote.Cao@gmail.com", email);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
